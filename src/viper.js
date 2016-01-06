89db9e53ea2e2306ca049c93807e9df989a4979f
Matthew Maynes <matthewmaynes1@gmail.com>
2016-01-06 15:43:45 -0500

    Added description of Versionable class

--- src/viper.js
+++ src/viper.js
@@ -1,6 +1,26 @@
 
+/**
+ * A decorator for a class that will add version information to an instance of
+ * an object. It provides instance methods for creating static representations
+ * of an instance object. There is also a factory method provided for generating
+ * an instance of an object using the static representation.
+ *
+ * @version 0.0.0
+ */
 var Versionable = (function(){
-    var Version = (function(){})();
+
+    /**
+     *
+     */
+    var Version = (function(){
+
+
+
+        return {
+
+        };
+    })();
+
 
     function Versionable (){}
 
