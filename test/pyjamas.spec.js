0602f136873b31da84820385514e58500326c8a6
Matthew Maynes <matthewmaynes1@gmail.com>
2016-01-13 13:42:17 -0500

    Added test fixtures for major.minor.patch Versions

--- test/pyjamas.spec.js
+++ test/pyjamas.spec.js
@@ -12,15 +12,58 @@ describe('Pyjamas', function(){
 
             expect(Pyjamas.Version.split).toThrow();
 
-            // TODO: When Pyjamas.Version.split is implemented then
-            // these test should be re-added
-            /*
+            // TODO Uncomment when Pyjamas.Version.split is done
 
+            /*
             expect(Pyjamas.Version.split('1.2.3')).toEqual([1, 2, 3]);
             expect(Pyjamas.Version.split('0.0.0')).toEqual([0, 0, 0]);
-            expect(Pyjamas.Version.split('1.10.100;)).toEqual([1, 10, 100]);
+            expect(Pyjamas.Version.split('1.10.100')).toEqual([1, 10, 100]);
+            expect(Pyjamas.Version.split('')).toEqual([0, 0, 0]);
             */
         });
+
+        it('Returns the major version number of a version string', function(){
+
+            expect(Pyjamas.Version.major).toThrow();
+
+            // TODO Uncomment when Pyjamas.Version.major is done
+
+            /*
+                expect(Pyjamas.Version.major('1.2.3')).toEqual(1);
+                expect(Pyjamas.Version.major('0.0.0')).toEqual(0);
+                expect(Pyjamas.Version.major('1.10.100')).toEqual(1);
+                expect(Pyjamas.Version.major('')).toEqual(0);
+            */
+        });
+
+        it('Returns the minor version number of a version string', function(){
+
+            expect(Pyjamas.Version.minor).toThrow();
+
+            // TODO Uncomment when Pyjamas.Version.minor is done
+
+            /*
+                expect(Pyjamas.Version.minor('1.2.3')).toEqual(2);
+                expect(Pyjamas.Version.minor('0.0.0')).toEqual(0);
+                expect(Pyjamas.Version.minor('1.10.100')).toEqual(10);
+                expect(Pyjamas.Version.minor('')).toEqual(0);
+            */
+        });
+
+        it('Returns the patch version number of a version string', function(){
+
+            expect(Pyjamas.Version.patch).toThrow();
+
+            // TODO Uncomment when Pyjamas.Version.patch is done
+
+            /*
+                expect(Pyjamas.Version.patch('1.2.3')).toEqual(3);
+                expect(Pyjamas.Version.patch('0.0.0')).toEqual(0);
+                expect(Pyjamas.Version.patch('1.10.100')).toEqual(100);
+                expect(Pyjamas.Version.patch('')).toEqual(0);
+            */
+        });
+
 
     });
 
